7c67c81cbb33c4e7520d342c1e9660e9c439fb99
XFaonAE <xfaonae@protonmail.com>
2022-05-09 00:33:14 -0400

    feat: Dir tree render

--- components/documentation/DocumentationSideBar.tsx
+++ components/documentation/DocumentationSideBar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { Response as SResponse } from "../../pages/api/v1/documentation/allRoutes";
 
 export default function DocumentationSideBar() {
-    const [records, setRecords] = useState<SResponse[]>([]);
+    const [records, setRecords] = useState<SResponse["pages"]>([]);
 
     useEffect(() => {
         setRecords([]);
@@ -17,15 +17,39 @@ export default function DocumentationSideBar() {
     // TODO: Implement loading bar/spinner
     console.log(records);
 
-    const Record = (settings: {}) => {
-        return (
-            
-        );
-    }
+    // records is mapped to the following
+    // interface Record {
+    //     path: string;
+    //     type: "directory" | "file";
+    //     name: string;
+    //     children?: Record[];
+    // }
+    // Render it with recursion and <details>
 
     return (
         <div>
-            
+            {
+                records.map(record => {
+                    const recursive = (record: SResponse["pages"][0]) => {
+                        if (record.children) {
+                            return (
+                                <details>
+                                    <summary>{record.name}</summary>
+                                    {record.children.map(recursive)}
+                                </details>
+                            );
+                        } else {
+                            return (
+                                <details>
+                                    <summary>{record.name}</summary>
+                                </details>
+                            );
+                        }
+                    }
+                    
+                    return recursive(record);
+                })
+            }
         </div>
     );
 }
