59d7571ee2e3288c5b1bdd586946156aa5334d2d
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-07-09 07:28:00 -0400

    refactor: Init Svelte project with sources

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let name = "a";
+</script>
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={() => name = "b"}>Change name</button>
