a7adacf1f0e72db38d59af1669fc34cdfbc66e49
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-07-12 19:03:18 -0400

    feat: Added footer component

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,9 +1,23 @@
 <div class="footer">
-
+    <div class="inner">
+        Hello <button>Hi</button>
+    </div>
 </div>
 
 <style lang="scss">
 	@import "../Color";
 
+    .footer {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        padding: 0 40px;
+        color: $text3;
+        border-top: 1px solid $dynamicLayer2;
 
+      .inner {
+        width: 100%;
+        max-width: 1500px;
+      }
+    }
 </style>
